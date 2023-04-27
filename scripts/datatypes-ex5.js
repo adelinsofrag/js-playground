31dc81d63cef93e603a5cada4f97dc8e2b020ea9
adelinsofrag <adelin.sofrag@gmail.com>
2023-04-27 19:28:18 +0300

    Splitting these exercises into individual file for a clear view of what happens there.

---
+++ scripts/datatypes-ex5.js
@@ -0,0 +1,10 @@
+
+
+
+/* -------- // Exercițiul 5: -------- */
+console.log("----- Exercițiul 5: -----");
+
+// RO: Inițializează o variabilă de tip null și afișează valoarea acesteia.
+// EN: Initialize a null variable and display its value.
+var myNull = null; // RO: variabila nulă  EN: null variable
+console.log(myNull); // output: null
---
+++ scripts/datatypes-ex6.js
@@ -0,0 +1,16 @@
+/* ---------------------------------- */
+/*      Interesting fact about JS     */
+/* ---------------------------------- */
+console.log("----- 🤔 Interesting fact about JS -----");
+
+// Concatenare între string și int
+var x = 10; // declară variabila x și atribuie valoarea 10 (un int)
+var y = "20"; // declară variabila y și atribuie valoarea "20" (un string)
+
+console.log(x + y); // afișează "1020" - JavaScript concatenează string-ul cu int-ul, în loc să adune valorile numerice
+
+// Comparare între string și int
+var x = 10; // declară variabila x și atribuie valoarea 10 (un int)
+var y = "5"; // declară variabila y și atribuie valoarea "5" (un string)
+
+console.log(x > y); // afișează true - JavaScript convertește valoarea lui y (un string) într-un int, pentru a putea face comparația
