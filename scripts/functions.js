4da7c6fd8bf05882ca21fd91e83285bb92bc7a72
adelinsofrag <adelin.sofrag@gmail.com>
2023-05-10 21:13:59 +0300

    Already presented functions.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,26 +1,141 @@
 /* ---------------------------------- */
 /*         +, -, /, * 2 numere        */
 /* ---------------------------------- */
+// function sum(firstNumber, secondNumber) {
+//   var sum = firstNumber + secondNumber;
+//   return sum;
+// }
+
+// function subtraction(firstNumber, secondNumber) {
+//   return firstNumber - secondNumber;
+// }
+
+// Operatie care
+// Primeste primii 2 parametrii ca fiind numerele cu care facem operatie.
+// Primeste al 3-lea parametru ca fiind tipul de operatie.
+function calcOperation(firstNumber, secondNumber, operationType) {
+  var result;
+
+  switch (operationType) {
+    case "+":
+      result = firstNumber + secondNumber;
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+    case "/":
+      result = firstNumber / secondNumber;
+      break;
+    case "*":
+      result = firstNumber * secondNumber;
+      break;
+    default:
+      result = "Nu ai ales tipul de operatie - vezi al 3-lea parametru!";
+      break;
+  }
+
+  console.log("test");
+
+  return result;
+}
+
+// testing.
+// console.log(calcOperation(2, 3, '*'));
+
 /* ---------------------------------- */
-/*          min, max 2 numere         */
+/*          max 2 numere         */
 /* ---------------------------------- */
+function maxBetween2Numbers(firstNumber, secondNumber) {
+  if (firstNumber > secondNumber) {
+    return firstNumber;
+  } else if (secondNumber > firstNumber) {
+    return secondNumber;
+  }
+
+  return firstNumber + "=" + secondNumber;
+}
 /* ---------------------------------- */
 /*         par, impar 1 numar         */
 /* ---------------------------------- */
+function isEven(number) {
+  return number % 2 == 0;
+}
+function isOdd(number) {
+  return number % 2 != 0;
+}
 /* ---------------------------------- */
 /*         restul de plata a%b        */
 /* ---------------------------------- */
+function restOf2Numbers(firstNumber, secondNumber) {
+  return firstNumber % secondNumber;
+}
 /* ---------------------------------- */
 /*        min --> max interval        */
 /* ---------------------------------- */
+function showIntervalBetween2Numbers(min, max) {
+  if (min > max) {
+    return min + " > " + max;
+  } else if (min == max) {
+    return min + " = " + max;
+  }
+
+  var result = "";
+
+  for (i = min; i <= max; i++) {
+    result = String(result) + " " + i;
+    // console.log(result, 'now i = :', i);
+  }
+
+  return result;
+}
 /* ---------------------------------- */
 /*   Fn care rezolva cazurile 👆🏻.     */
 /*   param: care din operatii ruleaza     */
 /*   by default: afiseaza tot        */
 /* ---------------------------------- */
+function resolveOperation(whichOperationType, firstNumber, secondNumber) {
+  var result;
 
+  switch (whichOperationType) {
+    case "+":
+      result = calcOperation(firstNumber, secondNumber, "+");
+      break;
+    case "-":
+      result = calcOperation(firstNumber, secondNumber, "-");
+      break;
+    case "/":
+      result = calcOperation(firstNumber, secondNumber, "/");
+      break;
+    case "*":
+      result = calcOperation(firstNumber, secondNumber, "*");
+      break;
+    case "rest":
+      result = restOf2Numbers(firstNumber, secondNumber, whichOperationType);
+      break;
+    case "even":
+      result = isEven(firstNumber);
+      break;
+    case "odd":
+      result = isOdd(firstNumber);
+      break;
+    case "max":
+      result = maxBetween2Numbers(firstNumber, secondNumber);
+      break;
+    case "interval":
+      result = showIntervalBetween2Numbers(firstNumber, secondNumber);
+      break;
+    default:
+      result = "Please add the first param - operation type!";
+      console.warn(result);
+      break;
+  }
+
+  return result;
+}
+/* ---------------------------------- */
+/*        Exemplul 1 de la curs       */
+/* ---------------------------------- */
 var introducere = "Hello! Urmeaza cantitatile retetei!";
-
 /**
  * Reteta.
  * preia ingredientele - amount
@@ -70,5 +185,3 @@ function cookPasta(nrPersoane) {
       nrPersoane * salt
   );
 }
-
-var retetaOutputPentru5Persoane = cookPasta();
