9878f91156ac28b76d23b7da1fa4f0daef0e14eb
surfthecode <surfthecode@gmail.com>
2023-05-10 20:54:34 +0300

    create math operations function

--- scripts/functions-class.js
+++ scripts/functions-class.js
@@ -1,11 +1,3 @@
-/**
-* @param {*} pastaAmount;
-* @param {*} oilAmount;
-* @param {*} garlicAmount;
-* @param {*} pepperAmount;
-* @param {*} saltAmount;
-
-*/
 function displayAmount(ingredientAmount, ingredientText) {
   let response;
 
@@ -32,3 +24,54 @@ function cookPasta(
 }
 
 cookPasta(450, 20, 10, 5, 1);
+
+console.log("----------------------------------------");
+
+const num1 = Number(prompt("Your first number"));
+const num2 = Number(prompt("Your second number"));
+const type = prompt("Type a choice: sum / substract / divide / multiply");
+let result = "";
+
+const sum = function (sum1, sum2) {
+  return sum1 + sum2;
+};
+
+const substract = function (sub1, sub2) {
+  return sub1 - sub2;
+};
+
+const divide = function (div1, div2) {
+  return div1 / div2;
+};
+
+const multiply = function (mul1, mul2) {
+  return mul1 * mul2;
+};
+
+function calcOperation(num1, num2, tpye) {
+  switch (type) {
+    case "sum":
+      result = sum(num1, num2);
+      break;
+    case "substract":
+      result = substract(num1, num2);
+      break;
+    case "divide":
+      result = divide(num1, num2);
+      break;
+    case "multiply":
+      result = multiply(num1, num2);
+      break;
+    case "":
+      result = "Chose an operation: sum / substract / divide / multiply";
+      break;
+    default:
+      result = "Chose an operation: sum / substract / divide / multiply";
+  }
+
+  return console.log(result);
+}
+
+calcOperation(num1, num2, type);
+
+console.log("----------------------------------------");
